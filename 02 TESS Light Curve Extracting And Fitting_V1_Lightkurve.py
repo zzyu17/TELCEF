268a8f187776b22b0830f37ba43d39f8ec758964
zzyu <zzyu06@163.com>
2025-06-05 15:16:18 +0800

    add transit upper clipping and corrected LightCurve saving & update documentation in 02 script

--- 02 TESS Light Curve Extracting And Fitting_V1_Lightkurve.py
+++ 02 TESS Light Curve Extracting And Fitting_V1_Lightkurve.py
@@ -44,12 +44,15 @@ exptime_in_days = (exptime * u.second).to(u.day).value  # convert exposure time 
 # set the coefficient of transit mask span manually
 transit_mask_raw_nans_removed_coefficient = 1.8
 
-clip = True
-sigma = 3.0
-
 flatten = True
 flatten_window_proportion = 0.05
 polyorder = 3
+
+clip = True
+clip_transit = True
+sigma_baseline = 3.0
+sigma_upper_transit = 0.4 # set sigma_upper_transit manually
+sigma_lower_transit = np.inf # set to np.inf to avoid clipping the real transit
 
 bin = True
 cadence_bin_size = 2.5
@@ -122,6 +125,12 @@ processed_lightcurve_plots_parent_dir = processed_lightcurve_plots_dir + f"/{nam
 os.makedirs(processed_lightcurve_plots_parent_dir, exist_ok=True)
 processed_lightcurve_plots_exptime_parent_dir = processed_lightcurve_plots_parent_dir + f"/Exposure Time={exptime}s"
 os.makedirs(processed_lightcurve_plots_exptime_parent_dir, exist_ok=True)
+
+eleanor_root = os.path.expanduser("~/.eleanor")
+eleanor_root_targetdata = eleanor_root + f"/targetdata"
+os.makedirs(eleanor_root_targetdata, exist_ok=True)
+eleanor_root_targetdata_source = eleanor_root_targetdata + f"/{name}"
+os.makedirs(eleanor_root_targetdata_source, exist_ok=True)
 
 
 
@@ -254,11 +263,13 @@ if flatten:
 
 
 else:
-    lc_flattened = lc_raw_nans_removed.copy()
-    lc_flattened_cdpp = calculate_cdpp(lc_flattened)
-
     lc_flattened_baseline = lc_raw_nans_removed_baseline.copy()
     lc_flattened_baseline_cdpp = calculate_cdpp(lc_flattened_baseline, transit_duration=transit_duration_raw_nans_removed_in_cadence)
+
+    lc_flattened_transit = lc_raw_nans_removed_transit.copy()
+
+    lc_flattened = lc_raw_nans_removed.copy()
+    lc_flattened_cdpp = calculate_cdpp(lc_flattened)
 
 
 
@@ -270,34 +281,68 @@ i += 1  # count the step
 if clip:
     # Clip and plot the baseline
     j = 1 # count the sub-step
-    lc_clipped_baseline = lc_flattened_baseline.remove_outliers(sigma=sigma)
+    lc_clipped_baseline = lc_flattened_baseline.remove_outliers(sigma=sigma_baseline)
     lc_clipped_baseline_cdpp = calculate_cdpp(lc_clipped_baseline, transit_duration=transit_duration_raw_nans_removed_in_cadence)
 
     lc_clipped_baseline_plot, ax_lc_clipped_baseline = plt.subplots(figsize=(20, 5))
     lc_clipped_baseline.errorbar(ax=ax_lc_clipped_baseline, label=f"simplified CDPP={lc_clipped_baseline_cdpp:.2f}")
-    ax_lc_clipped_baseline.set_title(f"{name} Sector {sector} {pipeline} {sigma} Sigma Clipped Baseline Exptime={exptime}s")
+    ax_lc_clipped_baseline.set_title(f"{name} Sector {sector} {pipeline} {sigma_baseline} Sigma Clipped Baseline Exptime={exptime}s")
     lc_clipped_baseline_plot.figure.tight_layout()
-    lc_clipped_baseline_plot.figure.savefig(processed_lightcurve_plots_exptime_parent_dir + f"/{i:02}-{j:01} {name} Sector {sector} {pipeline} {sigma} Sigma Clipped Baseline Exptime={exptime}s.png")
+    lc_clipped_baseline_plot.figure.savefig(processed_lightcurve_plots_exptime_parent_dir + f"/{i:02}-{j:01} {name} Sector {sector} {pipeline} {sigma_baseline} Sigma Clipped Baseline Exptime={exptime}s.png")
+
+
+    # Clip and plot the transit if there are upper outliers
+    if clip_transit:
+        j += 1  # count the sub-step
+        lc_flattened_baseline_nanmedian = np.nanmedian(lc_flattened_baseline.flux)
+        lc_clipped_transit = lc_flattened_transit.remove_outliers(cenfunc=lambda x, **kwargs: lc_flattened_baseline_nanmedian, sigma_lower=sigma_lower_transit, sigma_upper=sigma_upper_transit)  # remove the upper outliers in the transit based on the nanmedian of lc_flattened_baseline
+
+        lc_clipped_transit_plot, ax_lc_clipped_transit = plt.subplots(figsize=(20, 5))
+        lc_clipped_transit.errorbar(ax=ax_lc_clipped_transit)
+        ax_lc_clipped_transit.set_title(f"{name} Sector {sector} {pipeline} {sigma_upper_transit} Upper Sigma Clipped Transit Exptime={exptime}s")
+        lc_clipped_transit_plot.figure.tight_layout()
+        lc_clipped_transit_plot.figure.savefig(processed_lightcurve_plots_exptime_parent_dir + f"/{i:02}-{j:01} {name} Sector {sector} {pipeline} {sigma_upper_transit} Upper Sigma Clipped Transit Exptime={exptime}s.png")
+
+    else:
+        lc_clipped_transit = lc_flattened_transit.copy()
 
 
     # Retrieve and plot the clipped light curve
     j += 1 # count the sub-step
-    lc_clipped = lc_clipped_baseline.append(lc_flattened_transit)  # append the in-transit NaNs-removed raw light curve to the clipped baseline to retrieve the clipped light curve
+    lc_clipped = lc_clipped_baseline.append(lc_clipped_transit)  # append the in-transit NaNs-removed raw light curve to the clipped baseline to retrieve the clipped light curve
     lc_clipped_cdpp = calculate_cdpp(lc_clipped, transit_duration=transit_duration_raw_nans_removed_in_cadence)
 
     lc_clipped_plot, ax_lc_clipped = plt.subplots(figsize=(20, 5))
     lc_clipped.errorbar(ax=ax_lc_clipped, label=f"simplified CDPP={lc_clipped_cdpp:.2f}")
-    ax_lc_clipped.set_title(f"{name} Sector {sector} {pipeline} {sigma} Sigma Clipped Light Curve Exptime={exptime}s")
+    ax_lc_clipped.set_title(f"{name} Sector {sector} {pipeline} {sigma_baseline} Sigma Clipped Light Curve Exptime={exptime}s")
     lc_clipped_plot.figure.tight_layout()
-    lc_clipped_plot.figure.savefig(processed_lightcurve_plots_exptime_parent_dir + f"/{i:02}-{j:01} {name} Sector {sector} {pipeline} {sigma} Sigma Clipped Light Curve Exptime={exptime}s.png")
+    lc_clipped_plot.figure.savefig(processed_lightcurve_plots_exptime_parent_dir + f"/{i:02}-{j:01} {name} Sector {sector} {pipeline} {sigma_baseline} Sigma Clipped Light Curve Exptime={exptime}s.png")
 
 
 else:
     lc_clipped_baseline = lc_flattened_baseline.copy()
     lc_clipped_baseline_cdpp = calculate_cdpp(lc_clipped_baseline, transit_duration=transit_duration_raw_nans_removed_in_cadence)
 
+    lc_clipped_transit = lc_flattened_transit.copy()
+
     lc_clipped = lc_flattened.copy()
     lc_clipped_cdpp = calculate_cdpp(lc_clipped, transit_duration=transit_duration_raw_nans_removed_in_cadence)
+
+
+
+
+# Wrtie the corrected LightCurve into a .fits file
+if flatten and clip:
+    correction = "Flattened & Clipped"
+elif flatten and not clip:
+    correction = "Flattened"
+elif not flatten and clip:
+    correction = "Clipped"
+else:
+    correction = "Raw"
+
+
+lc_clipped.to_fits(eleanor_root_targetdata_source + f"/{name}_Sector {sector}_{method}_{pipeline}_{correction}_Exptime={exptime}s.fits", overwrite=True)
 
 
 
@@ -350,31 +395,30 @@ else:
 methodology_result_file = open(processed_lightcurve_plots_exptime_parent_dir + f"/{name} Sector {sector} {method} Exptime={exptime}s Methodologies And Results.txt", "w", encoding='utf-8')
 methodology_result_file.write(f"{name} Sector {sector} {method} Exptime={exptime}s Methodologies And Results\n\n")
 
+
 methodology_result_file.write("NASA Exoplanet Archive Source Planetary Parameters: \n"
                                     f"Period (Days): {period_NASA}\n"
                                     f"Epoch Time (BJD): {epoch_time_NASA}\n"
                                     f"Transit Duration (Days): {transit_duration_NASA}\n"
                                     f"Transit Depth: {transit_depth_NASA}\n\n")
 
+
 methodology_result_file.write("Lightkurve: \n\n")
 
-methodology_result_file.write(      f"Raw Light Curve CDPP: {lc_raw_cdpp}\n"
-                                    f"NaNs-Removed Raw Light Curve CDPP: {lc_raw_nans_removed_cdpp}\n"
-                                    f"BLS Fitted NaNs-Removed Raw Light Curve Period (Days): {period_raw_nans_removed}\n"
-                                    f"BLS Fitted NaNs-Removed Raw Light Curve Epoch Time (BTJD): {epoch_time_raw_nans_removed}\n"
-                                    f"BLS Fitted NaNs-Removed Raw Light Curve Transit Duration (Days): {transit_duration_raw_nans_removed}\n"
-                                    f"BLS Fitted NaNs-Removed Raw Light Curve Transit Duration (Cadences): {transit_duration_raw_nans_removed_in_cadence}\n"
-                                    f"BLS Fitted NaNs-Removed Raw Light Curve Transit Depth: {transit_depth_raw_nans_removed}\n"
-                                    f"Fitted the {name} Sector {sector} {pipeline} NaNs-Removed Raw Light Curve Exptime={exptime}s using the Box Least Squares (BLS) method in {pg_bls_raw_nans_removed_fitting_time} seconds.\n"
-                                    f"NaNs-Removed Raw Baseline CDPP: {lc_raw_nans_removed_baseline_cdpp}\n\n")
 
-methodology_result_file.write(      f"Clip: {clip}\n")
-if clip:
-    methodology_result_file.write(f"Sigma: {sigma}\n"
-                                  f"Clipped Baseline CDPP: {lc_clipped_baseline_cdpp}\n"
-                                  f"Clipped Light Curve CDPP: {lc_clipped_cdpp}\n\n")
-else:
-    methodology_result_file.write("\n") # write an empty line
+methodology_result_file.write(      f"Raw Light Curve CDPP: {lc_raw_cdpp}\n"
+                                    f"NaNs-Removed Raw Baseline CDPP: {lc_raw_nans_removed_baseline_cdpp}\n"
+                                    f"NaNs-Removed Raw Light Curve CDPP: {lc_raw_nans_removed_cdpp}\n\n")
+
+
+methodology_result_file.write(      f"Box Least Squares (BLS) Fitted NaNs-Removed Raw Light Curve Parameters: \n"
+                                    f"Period (Days): {period_raw_nans_removed}\n"
+                                    f"Epoch Time (BTJD): {epoch_time_raw_nans_removed}\n"
+                                    f"Transit Duration (Days): {transit_duration_raw_nans_removed}\n"
+                                    f"Transit Duration (Cadences): {transit_duration_raw_nans_removed_in_cadence}\n"
+                                    f"Transit Depth: {transit_depth_raw_nans_removed}\n"
+                                    f"Fitted the {name} Sector {sector} {pipeline} NaNs-Removed Raw Light Curve Exptime={exptime}s using the Box Least Squares (BLS) method in {pg_bls_raw_nans_removed_fitting_time} seconds.\n\n")
+
 
 methodology_result_file.write(      f"Flatten: {flatten}\n")
 if flatten:
@@ -386,9 +430,22 @@ if flatten:
 else:
     methodology_result_file.write("\n") # write an empty line
 
+
+methodology_result_file.write(      f"Clip: {clip}\n")
+if clip:
+    methodology_result_file.write(f"Baseline Sigma: {sigma_baseline}\n"
+                                  f"Transit Upper Sigma: {sigma_upper_transit}\n"
+                                  f"Transit Lower Sigma: {sigma_lower_transit}\n"
+                                  f"Clipped Baseline CDPP: {lc_clipped_baseline_cdpp}\n"
+                                  f"Clipped Light Curve CDPP: {lc_clipped_cdpp}\n\n")
+else:
+    methodology_result_file.write("\n") # write an empty line
+
+
 methodology_result_file.write(      f"Folded Period (Days): {period_raw_nans_removed}\n"
                                     f"Folded Epoch Time (BTJD): {epoch_time_raw_nans_removed}\n"
                                     f"Folded Light Curve CDPP: {lc_folded_cdpp}\n\n")
+
 
 methodology_result_file.write(      f"Bin: {bin}\n")
 if bin:
@@ -397,5 +454,6 @@ if bin:
                                   f"Binned Light Curve CDPP: {lc_binned_cdpp}\n\n")
 else:
     methodology_result_file.write("\n") # write an empty line
+
 
 methodology_result_file.close()
