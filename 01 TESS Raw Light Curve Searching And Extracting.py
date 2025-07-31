e63ed0345bb5720386c6e2505c2b54d51b8987f2
zzyu <zzyu06@163.com>
2025-07-31 20:36:56 +0800

    fix author indexes bug and make minor modifications in 01 script

--- 01 TESS Raw Light Curve Searching And Extracting.py
+++ 01 TESS Raw Light Curve Searching And Extracting.py
@@ -62,7 +62,7 @@ lc_search_result_file.write(str(lc_search_result))
 
 
 # Download and plot light curves of the assigned mission
-lc_collection_mission = lc_search_result[(np.char.find(lc_search_result.mission.astype(str), mission) >= 0)].download_all()
+lc_collection_mission = lc_search_result[(np.char.find(lc_search_result.mission.astype(str), mission) >= 0)].download_all() ##### add extra filtering conditions to exclude specific authors if their light curves are not supported #####
 print(f"Downloaded the {mission} light curves.\n")
 
 
@@ -72,6 +72,7 @@ for lc_mission in lc_collection_mission:
     lc_mission_normalized = lc_mission.normalize()
     lc_mission_normalized.plot(ax=ax_lc_collection_mission_normalized, label=f"{lc_mission_normalized.mission} {stage} {getattr(lc_mission_normalized, attribute)} Pipeline {lc_mission_normalized.author}, CDPP={lc_mission_normalized.estimate_cdpp():.2f}")
 ax_lc_collection_mission_normalized.set_title(f"{name} {mission} Normalized Raw Light Curves")
+ax_lc_collection_mission_normalized.legend(loc='upper right')
 lc_collection_mission_normalized_plot.figure.tight_layout()
 lc_collection_mission_normalized_plot.figure.savefig(raw_lightcurve_plots_mission_parent_dir + f"/{name} {mission} Normalized Raw Light Curves.png")
 print(f"Plotted the normalized {mission} raw light curve collection.\n")
@@ -91,16 +92,17 @@ lc_search_result_author_list = [
 ]
 
 for lc_mission_author, lcs_mission in lc_mission_authors_dict.items():
-    lc_mission_author_index = np.where(np.char.find(lc_search_result_author_list, lc_mission_author) >= 0)[0][0] # find the index in the search result table where the author matches for the first time
-    print(f"Found {len(lcs_mission)} light curve(s) for Pipeline {lc_mission_author} in the search result table, first of which matches at index {lc_mission_author_index}.")
+    lc_mission_author_index = np.where(np.char.find(lc_search_result_author_list, lc_mission_author) >= 0)[0] # find the indexes of search results whose authors match those in lc_mission_authors_dict
+    print(f"Found {len(lcs_mission)} light curve(s) for Pipeline {lc_mission_author} in the search result table, first of which matches at index {lc_mission_author_index[0]}.")
 
     for j in range(len(lcs_mission)):
         lc_mission = lcs_mission[j]
         # print(list(lc_mission.meta.keys()))
-        print(f"Plotting the single raw light curve for {mission} Pipeline {lc_mission.author} Exptime={lc_search_result.exptime.data[lc_mission_author_index + j]}s...")
+        print(f"Plotting the single raw light curve for {mission} Pipeline {lc_mission.author} Exptime={lc_search_result.exptime.data[lc_mission_author_index[j]]}s...")
         lc_mission_plot, ax_lc_mission = plt.subplots(figsize=(20, 5))
         lc_mission.plot(ax=ax_lc_mission, label=f"CDPP={lc_mission.estimate_cdpp():.2f}")
-        ax_lc_mission.set_title(f"{name} {mission} {stage} {getattr(lc_mission, attribute)} Pipeline {lc_mission.author} Raw Light Curve Exptime={lc_search_result.exptime.data[lc_mission_author_index + j]}s")
+        ax_lc_mission.set_title(f"{name} {mission} {stage} {getattr(lc_mission, attribute)} Pipeline {lc_mission.author} Raw Light Curve Exptime={lc_search_result.exptime.data[lc_mission_author_index[j]]}s")
+        ax_lc_mission.legend(loc='upper right')
         lc_mission_plot.figure.tight_layout()
-        lc_mission_plot.figure.savefig(raw_lightcurve_plots_mission_parent_dir + f"/{name} {mission} {stage} {getattr(lc_mission, attribute)} Pipeline {lc_mission.author} Raw Light Curve Exptime={lc_search_result.exptime.data[lc_mission_author_index + j]}s.png")
+        lc_mission_plot.figure.savefig(raw_lightcurve_plots_mission_parent_dir + f"/{name} {mission} {stage} {getattr(lc_mission, attribute)} Pipeline {lc_mission.author} Raw Light Curve Exptime={lc_search_result.exptime.data[lc_mission_author_index[j]]}s.png")
     print("")
